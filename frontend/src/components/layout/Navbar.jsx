8c851b32c93fe0a2d9d37a387ef08274d3985183
SheltonTeles <104696952+SheltonTeles@users.noreply.github.com>
2026-07-22 04:22:17 +0200

    Implement responsive mobile navigation drawer

--- frontend/src/components/layout/Navbar.jsx
+++ frontend/src/components/layout/Navbar.jsx
@@ -1,27 +1,130 @@
+import { useState } from "react";
 import { NavLink } from "react-router-dom";
-import logo from "../../assets/logos/logonobg.png";
+import logo from "../../assets/logoo-removebg-preview.png";
+import "./Navbar.css";
 
 function Navbar() {
+
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <nav className="navbar">
 
-      <div className="nav-left">
-        <img src={logo} alt="UniVault Logo" className="logo" />
+      {/* Logo */}
+      <div className="nav-logo">
+        <img src={logo} alt="UniVault Logo" />
       </div>
 
-      <div className="nav-center">
-        <ul>
-          <li><NavLink to="/">Home</NavLink></li>
-          <li><NavLink to="/resources">Resources</NavLink></li>
-          <li><NavLink to="/upload">Upload</NavLink></li>
-          <li><NavLink to="/about">About</NavLink></li>
-        </ul>
+
+      {/* Desktop Navigation */}
+      <div className="nav-links">
+
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/resources">Resources</NavLink>
+        <NavLink to="/upload">Upload</NavLink>
+        <NavLink to="/about">About</NavLink>
+
       </div>
 
-      <div className="nav-right">
-        <NavLink to="/login">Login</NavLink>
-        <NavLink to="/register">Register</NavLink>
+
+      {/* Desktop Auth */}
+      <div className="nav-auth">
+
+        <NavLink to="/login" className="login-btn">
+          Login
+        </NavLink>
+
+        <NavLink to="/register" className="register-btn">
+          Register
+        </NavLink>
+
       </div>
+
+
+      {/* Mobile Button */}
+      <button
+        className="menu-button"
+        onClick={() => setMenuOpen(true)}
+      >
+        ☰
+      </button>
+
+
+      {/* Mobile Drawer */}
+
+      <div className={`mobile-menu ${menuOpen ? "open" : ""}`}>
+
+        <button
+          className="close-button"
+          onClick={() => setMenuOpen(false)}
+        >
+          ✕
+        </button>
+
+
+        <NavLink 
+          to="/" 
+          onClick={() => setMenuOpen(false)}
+        >
+          Home
+        </NavLink>
+
+
+        <NavLink 
+          to="/resources"
+          onClick={() => setMenuOpen(false)}
+        >
+          Resources
+        </NavLink>
+
+
+        <NavLink 
+          to="/upload"
+          onClick={() => setMenuOpen(false)}
+        >
+          Upload
+        </NavLink>
+
+
+        <NavLink 
+          to="/about"
+          onClick={() => setMenuOpen(false)}
+        >
+          About
+        </NavLink>
+
+
+        <div className="mobile-divider"></div>
+
+
+        <NavLink 
+          to="/login"
+          className="login-btn"
+          onClick={() => setMenuOpen(false)}
+        >
+          Login
+        </NavLink>
+
+
+        <NavLink 
+          to="/register"
+          className="register-btn"
+          onClick={() => setMenuOpen(false)}
+        >
+          Register
+        </NavLink>
+
+      </div>
+
+
+      {/* Background overlay */}
+
+      {menuOpen && (
+        <div
+          className="overlay"
+          onClick={() => setMenuOpen(false)}
+        ></div>
+      )}
 
     </nav>
   );
